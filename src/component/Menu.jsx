9b0d5fae41cc5cc39b3c85bb53c38e582b39f8ce
WenBiming <m18077021652@163.com>
2021-11-07 16:26:29 +0800

    Improve menu and menu card. --story=1006401 --user=WenBiming . #1

--- src/component/Menu.jsx
+++ src/component/Menu.jsx
@@ -1,36 +1,46 @@
-import { Card, Col, Row, Space } from 'antd'
+import { Card, Col, Row, Avatar } from 'antd'
 import './Menu.css'
 import 'antd/dist/antd.css'
+import {
+    EditOutlined,
+    EllipsisOutlined,
+    SettingOutlined,
+} from '@ant-design/icons'
 
 export default function Menu() {
+    const { Meta } = Card
     return (
         <div className='site-card-wrapper'>
             <Row gutter={[16, { xs: 8, sm: 16, md: 24, lg: 32 }]}>
-                <Col span={6}>
-                    <Card title='Card title' bordered={true}>
-                        Card content
-                    </Card>
-                </Col>
-                <Col span={6}>
-                    <Card title='Card title' bordered={true}>
-                        Card content
-                    </Card>
-                </Col>
-                <Col span={6}>
-                    <Card title='Card title' bordered={true}>
-                        Card content
-                    </Card>
-                </Col>
-                <Col span={6}>
-                    <Card title='Card title' bordered={true}>
-                        Card content
-                    </Card>
-                </Col>
-                <Col span={6}>
-                    <Card title='Card title' bordered={true}>
-                        Card content
-                    </Card>
-                </Col>
+                {new Array(15).fill(null).map((_, index) => {
+                    const key = index + 1
+                    return (
+                        <Col span={6}>
+                            <Card
+                                // style={{ width: 300 }}
+                                cover={
+                                    <img
+                                        alt='example'
+                                        src='https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'
+                                    />
+                                }
+                                actions={[
+                                    <SettingOutlined key='setting' />,
+                                    <EditOutlined key='edit' />,
+                                    <EllipsisOutlined key='ellipsis' />,
+                                ]}
+                            >
+                                <Meta
+                                    avatar={
+                                        <Avatar src='https://joeschmoe.io/api/v1/random' />
+                                    }
+                                    title='Card title'
+                                    description='This is the description'
+                                />
+                            </Card>
+                        </Col>
+                    )
+                })}
             </Row>
         </div>
     )
